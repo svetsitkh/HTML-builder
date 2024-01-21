e029d519b1e1693ca25972bfab312a4fe6feca95
svetsitkh <svetsitkh@gmail.com>
2024-01-21 21:52:31 +0200

    feat: implement the 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.stat(path.join(file.path, file.name), (err, stats) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const fileSize = stats.size + 'b';
+            let fileName = file.name;
+            let fileExt = path.extname(fileName);
+            fileName = fileName.replace(fileExt, '');
+            fileExt = fileExt.replace('.', '');
+
+            console.log(fileName, '-', fileExt, '-', fileSize);
+          }
+        });
+      }
+    });
+  }
+});
